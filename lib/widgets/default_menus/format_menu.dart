821a5c5f73e3cb915e2eed8d2346e8ead9985567
Adrián Primo <adrianprimob@gmail.com>
2025-08-24 20:58:22 +0200

    :ambulance: Fixed some stuff I didn't notice, no need to worry

--- lib/widgets/default_menus/format_menu.dart
+++ lib/widgets/default_menus/format_menu.dart
@@ -6,80 +6,110 @@ class IPadFormatMenu extends IPadMenu {
   @override
   String get menuId => 'format';
 
-  IPadFormatMenu({List<PlatformMenuItem>? additionalItems})
-    : super(
-        label: 'Format',
-        menus: [
-          PlatformMenuWithIcon(
-            icon: CupertinoIcons.textformat,
-            label: 'Font',
-            menus: [
-              PlatformMenuItemGroup(
-                members: [
-                  PlatformMenuItemWithIcon(
-                    label: 'Bold',
-                    icon: CupertinoIcons.bold,
-                  ),
-                  PlatformMenuItemWithIcon(
-                    label: 'Italic',
-                    icon: CupertinoIcons.italic,
-                  ),
-                  PlatformMenuItemWithIcon(
-                    label: 'Underline',
-                    icon: CupertinoIcons.underline,
-                  ),
-                ],
-              ),
-              PlatformMenuItemGroup(
-                members: [
-                  PlatformMenuItemWithIcon(
-                    label: 'Bigger',
-                    icon: CupertinoIcons.textformat_superscript,
-                  ),
-                  PlatformMenuItemWithIcon(
-                    label: 'Smaller',
-                    icon: CupertinoIcons.textformat_subscript,
-                  ),
-                ],
-              ),
-            ],
-          ),
-          PlatformMenuWithIcon(
-            icon: CupertinoIcons.text_alignleft,
-            label: 'Text',
-            menus: [
-              PlatformMenuItemGroup(
-                members: [
-                  PlatformMenuItemWithIcon(
-                    label: 'Bold',
-                    icon: CupertinoIcons.bold,
-                  ),
-                  PlatformMenuItemWithIcon(
-                    label: 'Italic',
-                    icon: CupertinoIcons.italic,
-                  ),
-                  PlatformMenuItemWithIcon(
-                    label: 'Underline',
-                    icon: CupertinoIcons.underline,
-                  ),
-                ],
-              ),
-              PlatformMenuItemGroup(
-                members: [
-                  PlatformMenuItemWithIcon(
-                    label: 'Bigger',
-                    icon: CupertinoIcons.textformat_superscript,
-                  ),
-                  PlatformMenuItemWithIcon(
-                    label: 'Smaller',
-                    icon: CupertinoIcons.textformat_subscript,
-                  ),
-                ],
-              ),
-            ],
-          ),
+  IPadFormatMenu({
+    // Font
+    VoidCallback? onBold,
+    Intent? onBoldIntent,
+    VoidCallback? onItalic,
+    Intent? onItalicIntent,
+    VoidCallback? onUnderline,
+    Intent? onUnderlineIntent,
 
-          ...?additionalItems,
-        ],
-      );
+    // Size
+    VoidCallback? onBigger,
+    Intent? onBiggerIntent,
+    VoidCallback? onSmaller,
+    Intent? onSmallerIntent,
+
+    // Text Align
+    VoidCallback? onAlignLeft,
+    Intent? onAlignLeftIntent,
+    VoidCallback? onCenter,
+    Intent? onCenterIntent,
+    VoidCallback? onJustify,
+    Intent? onJustifyIntent,
+    VoidCallback? onAlignRight,
+    Intent? onAlignRightIntent,
+
+    List<PlatformMenuItem>? additionalItems,
+  }) : super(
+         label: 'Format',
+         menus: [
+           PlatformMenuWithIcon(
+             icon: CupertinoIcons.textformat,
+             label: 'Font',
+             menus: [
+               PlatformMenuItemGroup(
+                 members: [
+                   PlatformMenuItemWithIcon(
+                     label: 'Bold',
+                     icon: CupertinoIcons.bold,
+                     onSelected: onBold,
+                     onSelectedIntent: onBoldIntent,
+                   ),
+                   PlatformMenuItemWithIcon(
+                     label: 'Italic',
+                     icon: CupertinoIcons.italic,
+                     onSelected: onItalic,
+                     onSelectedIntent: onItalicIntent,
+                   ),
+                   PlatformMenuItemWithIcon(
+                     label: 'Underline',
+                     icon: CupertinoIcons.underline,
+                     onSelected: onUnderline,
+                     onSelectedIntent: onUnderlineIntent,
+                   ),
+                 ],
+               ),
+               PlatformMenuItemGroup(
+                 members: [
+                   PlatformMenuItemWithIcon(
+                     label: 'Bigger',
+                     icon: CupertinoIcons.textformat_superscript,
+                     onSelected: onBigger,
+                     onSelectedIntent: onBiggerIntent,
+                   ),
+                   PlatformMenuItemWithIcon(
+                     label: 'Smaller',
+                     icon: CupertinoIcons.textformat_subscript,
+                     onSelected: onSmaller,
+                     onSelectedIntent: onSmallerIntent,
+                   ),
+                 ],
+               ),
+             ],
+           ),
+           PlatformMenuWithIcon(
+             icon: CupertinoIcons.text_alignleft,
+             label: 'Text',
+             menus: [
+               PlatformMenuItemWithIcon(
+                 label: 'Align Left',
+                 icon: CupertinoIcons.text_alignleft,
+                 onSelected: onAlignLeft,
+                 onSelectedIntent: onAlignLeftIntent,
+               ),
+               PlatformMenuItemWithIcon(
+                 label: 'Center',
+                 icon: CupertinoIcons.text_aligncenter,
+                 onSelected: onCenter,
+                 onSelectedIntent: onCenterIntent,
+               ),
+               PlatformMenuItemWithIcon(
+                 label: 'Justify',
+                 icon: CupertinoIcons.text_justify,
+                 onSelected: onJustify,
+                 onSelectedIntent: onJustifyIntent,
+               ),
+               PlatformMenuItemWithIcon(
+                 label: 'Align Right',
+                 icon: CupertinoIcons.text_alignright,
+                 onSelected: onAlignRight,
+                 onSelectedIntent: onAlignRightIntent,
+               ),
+             ],
+           ),
+           ...?additionalItems,
+         ],
+       );
 }
--- lib/widgets/default_menus/view_menu.dart
+++ lib/widgets/default_menus/view_menu.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
+import 'package:ipados_menu_bar/ipados_menu_bar.dart';
 import 'package:ipados_menu_bar/widgets/default_menus/abstract_menu.dart';
 
 class IPadViewMenu extends IPadMenu {
   @override
   String get menuId => 'view';
 
-  IPadViewMenu({List<PlatformMenuItem>? additionalItems})
-    : super(label: 'View', menus: [...?additionalItems]);
+  IPadViewMenu({
+    VoidCallback? onShowSidebar,
+    Intent? onShowSidebarIntent,
+    List<PlatformMenuItem>? additionalItems,
+  }) : super(
+         label: 'View',
+         menus: [
+           PlatformMenuItemWithIcon(
+             icon: CupertinoIcons.sidebar_left,
+             label: 'Show Sidebar',
+             onSelected: onShowSidebar,
+             onSelectedIntent: onShowSidebarIntent,
+             shortcut: SingleActivator(
+               LogicalKeyboardKey.keyS,
+               shift: true, // Wrong key, I know...
+               meta: true,
+             ),
+           ),
+           ...?additionalItems,
+         ],
+       );
 }
