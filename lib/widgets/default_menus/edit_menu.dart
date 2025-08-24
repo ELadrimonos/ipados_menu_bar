15a0a9c5133c95ca0594bd662cb5f39769353f8c
Adrián Primo <adrianprimob@gmail.com>
2025-08-24 20:35:06 +0200

    :lipstick: Added icons to the edit menu items

--- lib/widgets/default_menus/edit_menu.dart
+++ lib/widgets/default_menus/edit_menu.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
+import 'package:ipados_menu_bar/ipados_menu_bar.dart';
 import 'package:ipados_menu_bar/widgets/default_menus/abstract_menu.dart';
 
 // TODO: Shortcuts & Emojis
 class IPadEditMenu extends IPadMenu {
-
   @override
   String get menuId => 'edit';
 
@@ -61,135 +61,152 @@ class IPadEditMenu extends IPadMenu {
     // Additional
     List<PlatformMenuItem>? additionalItems,
   }) : super(
-    label: 'Edit',
-    menus: [
-      PlatformMenuItemGroup(
-        members: [
-          PlatformMenuItem(
-            label: 'Undo',
-            onSelected: onUndo,
-            onSelectedIntent: onUndoIntent,
-          ),
-          PlatformMenuItem(
-            label: 'Redo',
-            onSelected: onRedo,
-            onSelectedIntent: onRedoIntent,
-          ),
-        ],
-      ),
-      PlatformMenuItem(
-        label: 'Cut',
-        onSelected: onCut,
-        onSelectedIntent: onCutIntent,
-      ),
-      PlatformMenuItem(
-        label: 'Copy',
-        onSelected: onCopy,
-        onSelectedIntent: onCopyIntent,
-      ),
-      PlatformMenuItem(
-        label: 'Paste',
-        onSelected: onPaste,
-        onSelectedIntent: onPasteIntent,
-      ),
-      PlatformMenuItem(
-        label: 'Paste with the same style',
-        onSelected: onPasteStyle,
-        onSelectedIntent: onPasteStyleIntent,
-      ),
-      PlatformMenuItem(
-        label: 'Delete',
-        onSelected: onDelete,
-        onSelectedIntent: onDeleteIntent,
-      ),
-      PlatformMenuItem(
-        label: 'Select All',
-        onSelected: onSelectAll,
-        onSelectedIntent: onSelectAllIntent,
-      ),
-      PlatformMenuItemGroup(
-        members: [
-          PlatformMenu(
-            label: 'Find',
-            menus: [
-              PlatformMenuItemGroup(
-                members: [
-                  PlatformMenuItem(
-                    label: 'Find',
-                    onSelected: onFind,
-                    onSelectedIntent: onFindIntent,
-                  ),
-                  PlatformMenuItem(
-                    label: 'Find & Replace',
-                    onSelected: onFindAndReplace,
-                    onSelectedIntent: onFindAndReplaceIntent,
-                  ),
-                  PlatformMenuItem(
-                    label: 'Find Next',
-                    onSelected: onFindNext,
-                    onSelectedIntent: onFindNextIntent,
-                  ),
-                  PlatformMenuItem(
-                    label: 'Find Previous',
-                    onSelected: onFindPrevious,
-                    onSelectedIntent: onFindPreviousIntent,
-                  ),
-                ],
-              ),
-              PlatformMenuItem(
-                label: 'Use Selection for Find',
-                onSelected: onUseSelectionForFind,
-                onSelectedIntent: onUseSelectionForFindIntent,
-              ),
-            ],
-          ),
-        ],
-      ),
-      PlatformMenu(
-        label: 'AutoFill',
-        menus: [
-          PlatformMenuItemGroup(
-            members: [
-              PlatformMenuItem(
-                label: 'Contact',
-                onSelected: onAutoFillContact,
-                onSelectedIntent: onAutoFillContactIntent,
-              ),
-              PlatformMenuItem(
-                label: 'Passwords',
-                onSelected: onAutoFillPasswords,
-                onSelectedIntent: onAutoFillPasswordsIntent,
-              ),
-              PlatformMenuItem(
-                label: 'Credit Card',
-                onSelected: onAutoFillCreditCard,
-                onSelectedIntent: onAutoFillCreditCardIntent,
-              ),
-            ],
-          ),
-          PlatformMenuItem(
-            label: 'Scan Text',
-            onSelected: onScanText,
-            onSelectedIntent: onScanTextIntent,
-          ),
-        ],
-      ),
-      PlatformMenuItem(
-        label: 'Dictation',
-        onSelected: onDictation,
-        onSelectedIntent: onDictationIntent,
-      ),
-      PlatformMenuItem(
-        label: 'Emoji',
-        onSelected: onEmoji,
-        onSelectedIntent: onEmojiIntent,
-      ),
-      PlatformMenuItem(
-        label: 'Show Keyboard',
-        onSelected: onShowKeyboard,
-        onSelectedIntent: onShowKeyboardIntent,
-      ),
-      ...?additionalItems,
-    ],
-  );
+         label: 'Edit',
+         menus: [
+           PlatformMenuItemGroup(
+             members: [
+               PlatformMenuItemWithIcon(
+                 icon: CupertinoIcons.arrow_uturn_left,
+                 label: 'Undo',
+                 onSelected: onUndo,
+                 onSelectedIntent: onUndoIntent,
+               ),
+               PlatformMenuItemWithIcon(
+                 icon: CupertinoIcons.arrow_uturn_right,
+                 label: 'Redo',
+                 onSelected: onRedo,
+                 onSelectedIntent: onRedoIntent,
+               ),
+             ],
+           ),
+           PlatformMenuItemWithIcon(
+             icon: CupertinoIcons.scissors,
+             label: 'Cut',
+             onSelected: onCut,
+             onSelectedIntent: onCutIntent,
+           ),
+           PlatformMenuItemWithIcon(
+             icon: CupertinoIcons.doc_on_doc,
+             label: 'Copy',
+             onSelected: onCopy,
+             onSelectedIntent: onCopyIntent,
+           ),
+           PlatformMenuItemWithIcon(
+             icon: CupertinoIcons.doc_on_clipboard,
+             label: 'Paste',
+             onSelected: onPaste,
+             onSelectedIntent: onPasteIntent,
+           ),
+           PlatformMenuItemWithIcon(
+             icon: CupertinoIcons.doc_on_clipboard_fill,
+             label: 'Paste with the same style',
+             onSelected: onPasteStyle,
+             onSelectedIntent: onPasteStyleIntent,
+           ),
+           PlatformMenuItemWithIcon(
+             icon: CupertinoIcons.trash,
+             label: 'Delete',
+             onSelected: onDelete,
+             onSelectedIntent: onDeleteIntent,
+           ),
+           PlatformMenuItemWithIcon(
+             icon: CupertinoIcons.textbox,
+             label: 'Select All',
+             onSelected: onSelectAll,
+             onSelectedIntent: onSelectAllIntent,
+           ),
+           PlatformMenuItemGroup(members: [...?additionalItems]),
+           PlatformMenuItemGroup(
+             members: [
+               PlatformMenuWithIcon(
+                 icon: CupertinoIcons.doc_text_search,
+                 label: 'Find',
+                 menus: [
+                   PlatformMenuItemGroup(
+                     members: [
+                       PlatformMenuItem(
+                         label: 'Find',
+                         onSelected: onFind,
+                         onSelectedIntent: onFindIntent,
+                       ),
+                       PlatformMenuItem(
+                         label: 'Find & Replace',
+                         onSelected: onFindAndReplace,
+                         onSelectedIntent: onFindAndReplaceIntent,
+                       ),
+                       PlatformMenuItem(
+                         label: 'Find Next',
+                         onSelected: onFindNext,
+                         onSelectedIntent: onFindNextIntent,
+                       ),
+                       PlatformMenuItem(
+                         label: 'Find Previous',
+                         onSelected: onFindPrevious,
+                         onSelectedIntent: onFindPreviousIntent,
+                       ),
+                     ],
+                   ),
+                   PlatformMenuItem(
+                     label: 'Use Selection for Find',
+                     onSelected: onUseSelectionForFind,
+                     onSelectedIntent: onUseSelectionForFindIntent,
+                   ),
+                 ],
+               ),
+             ],
+           ),
+           PlatformMenuWithIcon(
+             icon: CupertinoIcons.pencil_ellipsis_rectangle,
+             label: 'AutoFill',
+             menus: [
+               PlatformMenuItemGroup(
+                 members: [
+                   PlatformMenuItemWithIcon(
+                     icon: CupertinoIcons.person_circle,
+                   label: 'Contact',
+                     onSelected: onAutoFillContact,
+                     onSelectedIntent: onAutoFillContactIntent,
+                   ),
+                   PlatformMenuItemWithIcon(
+                     icon: CupertinoIcons.lock_circle,
+                     label: 'Passwords',
+                     onSelected: onAutoFillPasswords,
+                     onSelectedIntent: onAutoFillPasswordsIntent,
+                   ),
+                   PlatformMenuItemWithIcon(
+                     icon: CupertinoIcons.creditcard,
+                     label: 'Credit Card',
+                     onSelected: onAutoFillCreditCard,
+                     onSelectedIntent: onAutoFillCreditCardIntent,
+                   ),
+                 ],
+               ),
+               PlatformMenuItemWithIcon(
+                 icon: CupertinoIcons.doc_text_viewfinder,
+                 label: 'Scan Text',
+                 onSelected: onScanText,
+                 onSelectedIntent: onScanTextIntent,
+               ),
+             ],
+           ),
+           PlatformMenuItemWithIcon(
+             icon: CupertinoIcons.mic,
+             label: 'Dictation',
+             onSelected: onDictation,
+             onSelectedIntent: onDictationIntent,
+           ),
+           PlatformMenuItemWithIcon(
+             icon: CupertinoIcons.smiley,
+             label: 'Emoji',
+             onSelected: onEmoji,
+             onSelectedIntent: onEmojiIntent,
+           ),
+           PlatformMenuItemWithIcon(
+             icon: CupertinoIcons.keyboard,
+             label: 'Show Keyboard',
+             onSelected: onShowKeyboard,
+             onSelectedIntent: onShowKeyboardIntent,
+           ),
+         ],
+       );
 }
